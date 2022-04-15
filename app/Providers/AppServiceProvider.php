42f453d69f24a0765a75ae80cddf978ff4b6e834
Albert <albertbr2008@gmail.com>
2022-04-15 09:43:47 -0300

    First Commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Models\{
+    Category,
+};
+use App\Observers\{
+    CategoryObserver,
+};
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Category::observe(CategoryObserver::class);
+    }
+}
